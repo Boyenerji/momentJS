889737adac578b64ca351238efa5f63dfafaf090
bratik026 <boyenerji2014@yandex.ru>
2022-05-23 06:04:43 +0300

    new  date

---
+++ server.js
@@ -0,0 +1,12 @@
+// const express = require('express');
+// const path = require('path');
+
+// const app = express();
+
+// app.listen(3000);
+
+// const createPath = (page) => path.resolve(__dirname, 'views', `${page}.html`);
+
+// app.get('/', function (req, res) {
+//     res.sendFile(createPath('index'));
+// })
