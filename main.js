874e884c6db37cbe5226c2bc341d82dd976c2059
bratik026 <boyenerji2014@yandex.ru>
2022-08-04 22:22:23 +0300

    update

--- main.js
+++ main.js
@@ -55,6 +55,13 @@ dataOt.innerHTML = `${dayA.format('LLL')}`;
 // text.innerHTML = `${dayA.fromNow()}`;
 
 
+
+function randomInteger(min, max) {
+    let rand = min - 0.5 + Math.random() * (max - min + 1);
+    return Math.round(rand);
+}
+
+
 function testAnim() {
     text.animate(
     { 
