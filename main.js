8accc92649749cea77523a7d7f37f1cf1168eeb3
bratik026 <boyenerji2014@yandex.ru>
2022-08-15 23:09:27 +0300

    ffix date

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ const rules = [
         'Не фантазировать с утра.',
         'Не скачивать любое СЗ и Telegram.',
         'Не отключать скрытие картинок на пк.',
-        'Не заходить в сафари дома(импровизация и moment)',
+        'Не заходить в сафари дома(только moment)',
         'Запрет всего АСМР (кроме beautiful).',
         'Запрет поиска шурыгиной на ютубе.',
         'Не открыть э сцены на ютубе.',
@@ -45,7 +45,7 @@ rules.forEach((item, number) => {
 
 
 
-let dayA = moment("2022-07-30 18:23",       "YYYY-MM-DD HH:mm");
+let dayA = moment("2022-08-15 23:06",       "YYYY-MM-DD HH:mm");
 // let dayB = moment();
 
 // let duration = moment.duration(dayB.diff(dayA));
@@ -87,7 +87,7 @@ function dataPlain() {
     hours = duration._data.hours;
     mins = duration._data.minutes;
     sec = duration._data.seconds;
-    text.innerHTML = `${days + 'дн ' + hours + 'ч ' + mins + 'м ' + sec + 'с'}`;
+    text.innerHTML = `${days + 'дн ' + hours + 'ч ' + mins + 'м '}`;
 }
 
 // knopa.addEventListener('click', () => {
