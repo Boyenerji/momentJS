ecf62114ea5013d624ac1def0359b2fd503c9d28
bratik026 <boyenerji2014@yandex.ru>
2022-09-21 23:20:12 +0300

    big update

--- main.js
+++ main.js
@@ -19,12 +19,20 @@ console.log(moment().format('LL'));
 
 
 const 
+    body = document.querySelector('body'),
+    daysHtml = document.querySelector('#days'),
+    blockDuration = document.querySelector('.blockDuration'),
+    rulesHtml = document.querySelector('.rules'),
+    tests = document.querySelector('.tests'),
     text = document.querySelector('#text'),
+    DateOne = document.querySelector('#DateOne'),
+    DateTwo = document.querySelector('#DateTwo'),
     dataInput = document.querySelector('input'),
     dataOt = document.querySelector('#dataOt'),
     pRules = document.querySelector('#pRules'),
     knopa = document.querySelector('#knopa'),
     dateStart = document.querySelector('#dateStart'),
+    progress = document.querySelector('progress'),
     dateEnd = document.querySelector('#dateEnd');
 
 
@@ -47,28 +55,32 @@ const rules = [
 //     rulesHtml.innerHTML += `<p>${item}</p>`;
 // });
 
-let timerTwoTwo = setTimeout(() => dataPlainTwoTwo(), 0);
-function dataPlainTwoTwo() {
-    pRules.innerHTML = `${rules[randomInteger(0,10)]}`;
-}
+// let timerTwoTwo = setTimeout(() => dataPlainTwoTwo(), 0);
+// function dataPlainTwoTwo() {
+//     pRules.innerHTML = `${rules[randomInteger(0,10)]}`;
+// }
 
+// pRules.innerHTML = `${rules[randomInteger(0,10)]}`;
 let timerTwo = setInterval(() => dataPlainTwo(), 2000);
 function dataPlainTwo() {
     pRules.innerHTML = `${rules[randomInteger(0,10)]}`;
 }
 
 
+let dayBB = moment();
+const dayA = moment("2022-09-21 22:08",       "YYYY-MM-DD HH:mm"),
+      dayB = moment("2023-01-01 00:00",       "YYYY-MM-DD HH:mm");
 
 
+      
+DateOne.innerHTML = `${Math.round(moment.duration(dayBB.diff(dayA)).asHours())}`;
+DateTwo.innerHTML = `${Math.round(moment.duration(dayB.diff(dayA)).asHours())}`;
 
-let dayA = moment("2022-08-20 23:35",       "YYYY-MM-DD HH:mm");
-let dayAtest = moment("2022-08-20");
+// let test = moment.duration(dayB.diff(dayA));
+// DateTwo.innerHTML = 'test';
 
-console.log(dayAtest);
-// let dayB = moment();
-
-// let duration = moment.duration(dayB.diff(dayA));
-// console.log(duration);
+// console.log(moment.duration(dayB.diff(dayA)));
+// let dayAtest = moment("2022-08-20");
 
 dateStart.innerHTML = dayA.format('LLL');
 dateEnd.innerHTML = moment().format('LL');
@@ -99,24 +111,51 @@ function randomInteger(min, max) {
 
 let timerId = setInterval(() => dataPlain(), 1000);
 function dataPlain() {
-    let dayA = moment("2022-08-20 23:35",       "YYYY-MM-DD HH:mm");
-    let dayB = moment();
 
+
+    //Сколько часов от одной даты к другой
+
+    // let dayA = moment("2022-09-21 22:08",       "YYYY-MM-DD HH:mm");
+    // let dayB = moment("2023-01-01 00:00",       "YYYY-MM-DD HH:mm");
     let duration = moment.duration(dayB.diff(dayA));
+
+    progress.max = `${duration.asHours()}`;
+    console.dir(duration.asHours());
+
+    //Часов осталось от одной даты к другой
+    // let dayAA = moment("2022-09-21 22:08",       "YYYY-MM-DD HH:mm");
+    // let dayBB = moment();
+    let durationAB = moment.duration(dayBB.diff(dayA));
+    progress.value = durationAB.asHours();
+
+    console.log(durationAB.asHours());
+
     let 
-    days = duration._data.days,
-    hours = duration._data.hours;
-    mins = duration._data.minutes;
-    sec = duration._data.seconds;
+    days = durationAB._data.days,
+    hours = durationAB._data.hours;
+    mins = durationAB._data.minutes;
+    sec = durationAB._data.seconds;
     text.innerHTML = `${days + 'дн ' + hours + 'ч ' + mins + 'м '}`;
 }
 
-// knopa.addEventListener('click', () => {
-//     let 
-//     days = duration._data.days,
-//     hours = duration._data.hours;
-//     mins = duration._data.minutes;
-//     sec = duration._data.seconds;
-//     text.innerHTML = `${days + ' days ' + hours + ' hours' + mins + 'mins' + sec + 'sec'}`;
-// });
 
+DarkTheme();
+function DarkTheme() {
+    let date = new Date();
+    if (date.getHours() > 19 || date.getHours() < 6) {
+        body.style.backgroundColor = '#343a40';
+        // hello_h1.style.color = '#fff';
+        daysHtml.style.backgroundColor = '#495057';
+        daysHtml.style.color = '#FFFAFA';
+        tests.style.color = '#FFFAFA';
+        pRules.style.color = '#FFFAFA';
+        blockDuration.style.backgroundColor = '#495057';
+        rulesHtml.style.backgroundColor = '#495057';
+        // mainWord.style.color = '#fff';
+        // descWord.style.color = '#adb5bd';
+        // alert_info.style.backgroundColor = '#343a40';
+        // alert_info.style.color = '#fff';
+        // select.style.backgroundColor = '#6c757d';
+        // select.style.color = '#fff';
+    }
+}
