dae33ee2226c8e90b1d4fb2fa86518af3a584227
bratik026 <boyenerji2014@yandex.ru>
2022-12-19 23:38:53 +0300

    upd

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ app.set('view engine', 'ejs');
 
 
 app.use(express.json());
+app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 
