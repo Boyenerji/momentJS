34a5b79d1d26fd3a290fd0e237bd3ec33165f9c5
bratik026 <boyenerji2014@yandex.ru>
2022-12-20 09:51:42 +0300

    upd

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.post('/', (req, res) => {
 });
 
 
-app.listen(4444, (err) => {
+app.listen(process.env.PORT, (err) => {
     if (err) return console.log(err);
     console.log('Server ok');
 });
