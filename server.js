c7f4fa0747c441acc8b1f69508609321cf836e2d
bratik026 <boyenerji2014@yandex.ru>
2022-12-22 16:30:26 +0300

    upd: 22/12/22 16:30

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejs = require('ejs');
-require('dotenv').config();
+// require('dotenv').config();
 const morgan = require('morgan');
 const moment = require('moment');
-moment.locale('ru');  
+moment.locale('ru'); 
+// const validator = require('validator');
 mongoose.set('strictQuery', false);
 const UserModel = require('./models/User.js');
+
+
+
 
 
 mongoose
@@ -15,7 +19,7 @@ mongoose
     .then(() => console.log('DB OK'))
     .catch((err) => console.log('DB ERROR', err));
 
-const createPath = (page) => path.resolve(__dirname, '../views', `${page}.ejs`);
+const createPath = (page) => path.resolve(__dirname, 'ejs-views', `${page}.ejs`);
 const app = express();
 app.set('view engine', 'ejs');
 
@@ -28,36 +32,70 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
 
 app.get('/', (req, res) => {
     const title = 'Главная';
-    // const timeNow = moment();
+
+    // const datelol = '22.13.22 14dasdsadasdasdsa:30dsdasdsad';
+
+    // console.log(moment(datelol, 'DD.MM.YY HH:mm').format());
+
+    // console.log(moment([2022, 0, 21]))
+
+    // console.log(validator.isEmail('foo@bar.com'));
+    // res.render(createPath('test'));
+
+    const hours = 'hours';
+    const timeNow = moment();
+    
     // console.log(timeNow);
-    res.render(createPath('index'), { title })
-    // UserModel
-    // .find()
+    UserModel
+    .find()
     // .then(posts => console.log(posts))
-    // .then(posts => res.render(createPath('index'), { posts, title, timeNow, moment }))
-    // .catch((error) => {
-    //   console.log(error);
-    //   res.render(createPath('error'));
-    // });
+    .then(posts => res.render(createPath('index'), { posts, title, timeNow, moment }))
+    .catch((error) => {
+      console.log(error);
+      res.render(createPath('error'));
+    });
 });
 
 
-// app.post('/', (req, res) => {
-//     const { name, datevalue } = req.body;
-    
-//     console.log(name, datevalue);
+app.post('/', (req, res) => {
+    console.log(req.body);
+    let { name, datevalue } = req.body;
 
-//     UserModel
-//       .findByIdAndUpdate('639f383a3936a699209e974d', { name, datevalue })
-//       .then((result) => res.redirect('/'))
-//       .catch((error) => {
-//         console.log(error);
-//         res.render(createPath('error'), { title: 'Error' });
-//     });
+    if (moment(datevalue, 'DD.MM.YYYY HH:mm').isValid()) {
+      datevalue = moment(datevalue, 'DD.MM.YYYY HH:mm').format();
+      UserModel
+      .findByIdAndUpdate('639f383a3936a699209e974d', { name, datevalue })
+      .then((result) => res.redirect('/'))
+      .catch((error) => {
+          console.log(error);
+          res.render(createPath('error'), { title: 'Error' });
+      });
+    } else {
+      title = 'Ошибка';
+      res
+      .status(404)
+      .render(createPath('error'), { title });
+    }
+
+});
+
+
+app.use((req, res) => {
+    const title = 'Error Page';
+    res
+      .status(404)
+      .render(createPath('error'), { title });
+});
+  
+
+
+// app.listen(50289, (err) => {
+//     if (err) return console.log(err);
+//     console.log(`listening port ${50289}`);
 // });
 
 
-app.listen(process.env.PORT, (err) => {
+app.listen(4444, (err) => {
     if (err) return console.log(err);
-    console.log(`listening port ${process.env.PORT}`);
+    console.log(`listening port ${4444}`);
 });
